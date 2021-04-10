4ebe1f1a31bdf4032e08080edafb4cc2afbc65ef
Nickolay <mr-nicko89@yandex.ru>
2021-04-10 17:51:35 +0300

    Экспериментирую по курсу от alishev. Написал калькулятор. Отображается в представлении calculator.html

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -30,5 +30,31 @@ public class HelloController {
 		model.addAttribute("message", "Hello, "+ name + " " + surname);
 		return "goodbye";
 	}
+	@GetMapping(value = "/calculator")
+	public String calculator (@RequestParam(value = "a") int a, @RequestParam(value = "b") int b,
+							  @RequestParam(value = "action") String action, Model model){
+		double result;
+
+		switch (action){
+			case "multiplication":
+				result = a*b;
+				break;
+			case "division":
+				result = a/(double)b;
+				break;
+			case "subtraction":
+				result = a-b;
+				break;
+			case " addition":
+				result = a+b;
+				break;
+			default:
+				result = 0;
+				break;
+		}
+		model.addAttribute("result", result);
+		return "/calculator";
+	}
+
 	
 }

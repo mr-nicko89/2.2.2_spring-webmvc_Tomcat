09002d6b94e9458e0105f934a8fab07af3027aa9
Nickolay <mr-nicko89@yandex.ru>
2021-04-10 17:31:11 +0300

    Экспериментирую по курсу от alishev. Добавил ответ на запрос с параметрами в printGoodBay.

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/first")
+@RequestMapping("/")
 public class HelloController {
 
 	@GetMapping(value = "/hello")
@@ -22,7 +23,10 @@ public class HelloController {
 		return "index";
 	}
 	@GetMapping(value = "/goodbye")
-	public String pringGoodBay(ModelMap model){
+	public String printGoodBay(@RequestParam(value = "name",required = false) String name,
+							   @RequestParam(value = "surname", required = false) String surname){
+		System.out.println("Hello, "+ name + " " + surname);
+
 		return "goodbye";
 	}
 	
